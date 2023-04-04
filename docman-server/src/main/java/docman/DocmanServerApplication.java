2a27b4e129e35fbc76506034e8bf4c0f46392189
Larome-engineer <romanlacusta@gmail.com>
2023-04-04 13:50:56 +0300

    Создание серверной части проекта

---
+++ docman-server/src/main/java/docman/DocmanServerApplication.java
@@ -0,0 +1,13 @@
+package docman;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocmanServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DocmanServerApplication.class, args);
+    }
+
+}
---
+++ docman-server/src/test/java/docman/DocmanServerApplicationTests.java
@@ -0,0 +1,13 @@
+package docman;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocmanServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
